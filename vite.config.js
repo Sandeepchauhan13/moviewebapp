707a85fe0f16e555565bce26b2a53045069e7480
apnainstitute-demo <sinh.pozta1@gmail.com>
2023-12-16 18:03:11 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/reactnetflixclone/",
   server:{
     port: 3000,
   },
